567f60304741bcbf7407610f6f05111a463acc53
Kribouille <benjamin.vion1@gmail.com>
2014-08-06 11:23:32 +0200

    Création d'un stage
    Quelques modifis apportées aux pages html avec importation d'une page
    JavaScript sur le jeu, et création d'un stage délimitant le cadre du jeu
    (valeurs des délimitations à débattre en fonction du jeu ^^).

---
+++ pagesDesJeux/JS_jeu1_casseBrique.js
@@ -0,0 +1,31 @@
+﻿document.write("Page du jeu en question<br><br><br>Le jeu serai ici<br><br><br>");
+
+/* CB = Casse-Brique (et pas Carte Bancaire ^^)
+Cette variable regroupe toutes les variables globales du jeu et les fonctions associées. C'est juste pour structurer le code.)*/
+
+
+
+var cb = {
+	createGameStage: function (){
+        this.stage = new Kinetic.Stage({
+                container: 'container',
+                width: 1003,
+                height: 1003
+        });
+        
+        this.layer = new Kinetic.Layer(); // Layer = couche en anglais
+
+        this.rect = new Kinetic.Rect({ // Rectangle de base, le contour du jeu
+                x: 2,
+                y: 2,
+                width: 1000,
+                height: 1000,
+                stroke: 'black',
+                strokeWidth: 2
+        });
+        this.layer.add(this.rect);
+		this.stage.add(this.layer);
+	}
+};
+
+cb.createGameStage();
